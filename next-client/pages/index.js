aa851edca5ce7c095eb240bd64bc20824373665e
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-04-25 18:26:38 +0530

    Update index.js

--- next-client/pages/index.js
+++ next-client/pages/index.js
@@ -14,8 +14,8 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
         <br/>
-        <MdEditor></MdEditor>
       </main>
+      <MdEditor></MdEditor>
     </div>
   )
 }

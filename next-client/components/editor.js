392daed88808ff9f21f901f14a931eb64c938d35
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-04-25 23:07:44 +0530

    Update editor.js

--- next-client/components/editor.js
+++ next-client/components/editor.js
@@ -32,6 +32,7 @@ export default function Editor () {
             renderHTML={(text) => mdParser.render(text)}
             onChange={handleEditorChange}
             onImageUpload={handleImageUpload}
+            config={{view: {html: false}}}
         />
     )
 }
